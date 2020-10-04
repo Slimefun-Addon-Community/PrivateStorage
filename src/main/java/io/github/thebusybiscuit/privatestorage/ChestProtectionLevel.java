7aa232679331dc2c1ceaa7b3572082d06a970b18
LinoxGH <54643600+LinoxGH@users.noreply.github.com>
2020-10-04 12:06:34 +0300

    Some code refactor and fixing issues.

--- src/main/java/io/github/thebusybiscuit/privatestorage/ChestProtectionLevel.java
+++ src/main/java/io/github/thebusybiscuit/privatestorage/ChestProtectionLevel.java
@@ -1,8 +1,24 @@
 package io.github.thebusybiscuit.privatestorage;
 
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
 public enum ChestProtectionLevel {
-	
-	PUBLIC,
-	PRIVATE;
+    
+    /**
+     * A public chest can be accessed by any {@link Player}
+     * who has the permission to access chests in the {@link Location}.
+     * This way protection plugins can keep the chests safe.
+     */
+    PUBLIC,
+    
+    
+    /**
+     * A private chest can only be accessed by the very same {@link Player}
+     * who has also placed the chest down.
+     *
+     * Note: These chests cannot be used in cargo networks.
+     */
+    PRIVATE;
 
 }
