5af1bff3c859ef89d328ce258d425aac0424f18f
TheBusyBiscuit <mrCookieSlime@gmail.com>
2020-11-25 20:17:53 +0100

    Updated to latest Slimefun version

--- src/main/java/io/github/thebusybiscuit/privatestorage/PrivateStorage.java
+++ src/main/java/io/github/thebusybiscuit/privatestorage/PrivateStorage.java
@@ -1,5 +1,6 @@
 package io.github.thebusybiscuit.privatestorage;
 
+import org.bstats.bukkit.Metrics;
 import org.bukkit.NamespacedKey;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -8,41 +9,42 @@ import io.github.thebusybiscuit.privatestorage.storage.PublicChests;
 import io.github.thebusybiscuit.slimefun4.api.SlimefunAddon;
 import me.mrCookieSlime.CSCoreLibPlugin.general.Inventory.Item.CustomItem;
 import me.mrCookieSlime.Slimefun.Objects.Category;
-import me.mrCookieSlime.Slimefun.bstats.bukkit.Metrics;
 import me.mrCookieSlime.Slimefun.cscorelib2.config.Config;
 import me.mrCookieSlime.Slimefun.cscorelib2.skull.SkullItem;
 import me.mrCookieSlime.Slimefun.cscorelib2.updater.GitHubBuildsUpdater;
 import me.mrCookieSlime.Slimefun.cscorelib2.updater.Updater;
 
 public class PrivateStorage extends JavaPlugin implements SlimefunAddon {
-	
-	@Override
-	public void onEnable() {
-		Config cfg = new Config(this);
 
-		// Setting up bStats
-		new Metrics(this, 4912);
+    @Override
+    public void onEnable() {
+        Config cfg = new Config(this);
 
-		if (getDescription().getVersion().startsWith("DEV - ")) {
-			Updater updater = new GitHubBuildsUpdater(this, getFile(), "TheBusyBiscuit/PrivateStorage/master");
-			
-			// Only run the Updater if it has not been disabled
-			if (cfg.getBoolean("options.auto-update")) updater.start();
-		}
-		
-		Category category = new Category(new NamespacedKey(this, "private_storage"), new CustomItem(SkullItem.fromBase64("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZThlNTU0NGFmN2Y1NDg5Y2MyNzQ5MWNhNjhmYTkyMzg0YjhlYTVjZjIwYjVjODE5OGFkYjdiZmQxMmJjMmJjMiJ9fX0="), "&7Private Storage - Chests and Safes", "", "&a> Click to open"));
-		
-		new PublicChests(this, category);
-		new PrivateChests(this, category);
-	}
+        // Setting up bStats
+        new Metrics(this, 4912);
 
-	@Override
-	public JavaPlugin getJavaPlugin() {
-		return this;
-	}
+        if (getDescription().getVersion().startsWith("DEV - ")) {
+            Updater updater = new GitHubBuildsUpdater(this, getFile(), "TheBusyBiscuit/PrivateStorage/master");
 
-	@Override
-	public String getBugTrackerURL() {
-		return "https://github.com/TheBusyBiscuit/PrivateStorage/issues";
-	}
+            // Only run the Updater if it has not been disabled
+            if (cfg.getBoolean("options.auto-update")) {
+                updater.start();
+            }
+        }
+
+        Category category = new Category(new NamespacedKey(this, "private_storage"), new CustomItem(SkullItem.fromBase64("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZThlNTU0NGFmN2Y1NDg5Y2MyNzQ5MWNhNjhmYTkyMzg0YjhlYTVjZjIwYjVjODE5OGFkYjdiZmQxMmJjMmJjMiJ9fX0="), "&7Private Storage - Chests and Safes", "", "&a> Click to open"));
+
+        new PublicChests(this, category);
+        new PrivateChests(this, category);
+    }
+
+    @Override
+    public JavaPlugin getJavaPlugin() {
+        return this;
+    }
+
+    @Override
+    public String getBugTrackerURL() {
+        return "https://github.com/TheBusyBiscuit/PrivateStorage/issues";
+    }
 }
